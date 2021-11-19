510b891344ff480ab3436d25a0bbde1a32f63e02
Dipesh Patil <heydipesh28@gmail.com>
2021-11-20 01:03:29 +0530

    List Groups

---
+++ src/components/ListGroups/SimpleListGroupWithIconButton/SimpleListGroupWithIconButton.jsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+import PropTypes from "prop-types";
+
+import { ListGroup } from "react-bootstrap";
+
+import "./SimpleListGroupWithIconButton.scss";
+
+const acceptedIconButtonOrientationValues = ["start", "left", "end", "right"];
+
+const SimpleListGroupWithIconButton = ({
+  listData = [],
+  variant,
+  handleIconClick,
+  iconButtonOrientation,
+  iconButtonAdditionalClasses = [],
+}) => {
+  const isValidOrientationValue = acceptedIconButtonOrientationValues.includes(
+    iconButtonOrientation
+  );
+  const orientation = isValidOrientationValue
+    ? acceptedIconButtonOrientationValues
+        .slice(0, 2)
+        .includes(iconButtonOrientation)
+      ? "left"
+      : "right"
+    : "right";
+
+  return (
+    <ListGroup variant={variant}>
+      {listData.map((listItem, listIdx) => (
+        <ListGroup.Item key={listIdx}>
+          {listItem}
+          <span
+            onClick={() => handleIconClick(listIdx)}
+            className={`float-${orientation} icon-button px-1 ${clsx(
+              iconButtonAdditionalClasses
+            )}`}
+          >
+            <i className="bi bi-x-lg" />
+          </span>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
+
+SimpleListGroupWithIconButton.propTypes = {
+  listData: PropTypes.array,
+  variant: PropTypes.string,
+  handleIconClick: PropTypes.func,
+  iconButtonOrientation: PropTypes.string,
+  iconButtonAdditionalClasses: PropTypes.array,
+};
+
+export default SimpleListGroupWithIconButton;

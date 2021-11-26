99583d62901e661b053c421fb484d04b336feb5f
Dipesh Patil <heydipesh28@gmail.com>
2021-11-26 22:20:31 +0530

    Header optimisations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,47 +3,25 @@ import clsx from "clsx";
 
 import "./Header.scss";
 
-const DEFAULT_HEADER_CLASS = clsx(["roboto"]);
+const DEFAULT_HEADER_CLASS = ["roboto"];
 
 const Header = ({ text, size = 1, additionalClasses = [] }) => {
+  const headerClasses = clsx([...DEFAULT_HEADER_CLASS, ...additionalClasses]);
+  
   switch (parseInt(size)) {
     case 6:
-      return (
-        <h6 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h6>
-      );
+      return <h6 className={headerClasses}>{text}</h6>;
     case 5:
-      return (
-        <h5 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h5>
-      );
+      return <h5 className={headerClasses}>{text}</h5>;
     case 4:
-      return (
-        <h4 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h4>
-      );
+      return <h4 className={headerClasses}>{text}</h4>;
     case 3:
-      return (
-        <h3 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h3>
-      );
+      return <h3 className={headerClasses}>{text}</h3>;
     case 2:
-      return (
-        <h2 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h2>
-      );
+      return <h2 className={headerClasses}>{text}</h2>;
     case 1:
     default:
-      return (
-        <h1 className={`${DEFAULT_HEADER_CLASS} ${clsx(additionalClasses)}`}>
-          {text}
-        </h1>
-      );
+      return <h1 className={headerClasses}>{text}</h1>;
   }
 };
 

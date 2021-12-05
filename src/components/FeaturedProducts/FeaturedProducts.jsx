e95a815328bcdebb17a50cd327d22f92e6ccda8b
Dipesh Patil <heydipesh28@gmail.com>
2021-12-06 00:52:57 +0530

    Added FeaturedProduct Component

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+import Header from "../Header/Header";
+
+import featuredProductsData from "../../data/featured_products_data.json";
+import "./FeaturedProducts.scss";
+
+const FeaturedProducts = () => {
+  const [featuredProductInfo, setFeaturedProductInfo] = useState(
+    featuredProductsData[0]
+  );
+
+  return (
+    <Container className="mt-3">
+      <Header
+        size={1}
+        additionalClasses={["text-start", "readex-pro", "readex-pro__medium"]}
+      >
+        <i class="bi bi-arrow-left"></i>
+        &nbsp;Featured Products&nbsp;
+        <i class="bi bi-arrow-right"></i>
+      </Header>
+      <Row className="mt-3">
+        <div className="scrollmenu">
+          {featuredProductsData.map((product) => {
+            return (
+              <Col key={product.id} className="col-4 h-scroll" sm={2}>
+                <Image
+                  onClick={() => setFeaturedProductInfo(product)}
+                  className="featured-product"
+                  src={product.imgUrl}
+                />
+              </Col>
+            );
+          })}
+        </div>
+      </Row>
+      <Row className="mt-3">
+        <Col sm={4}>
+          <center>
+            <Image
+              className="featured-product-image"
+              src={featuredProductInfo.imgUrl}
+            />
+          </center>
+        </Col>
+        <Col className="readex-pro featured-product-info px-4 py-3" sm={8}>
+          {featuredProductInfo.description}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FeaturedProducts;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
@@ -5,23 +6,23 @@ import "./Header.scss";
 
 const DEFAULT_HEADER_CLASS = ["roboto"];
 
-const Header = ({ text, size = 1, additionalClasses = [] }) => {
+const Header = ({ children, size = 1, additionalClasses = [] }) => {
   const headerClasses = clsx([...DEFAULT_HEADER_CLASS, ...additionalClasses]);
 
   switch (parseInt(size)) {
     case 6:
-      return <h6 className={headerClasses}>{text}</h6>;
+      return <h6 className={headerClasses}>{children}</h6>;
     case 5:
-      return <h5 className={headerClasses}>{text}</h5>;
+      return <h5 className={headerClasses}>{children}</h5>;
     case 4:
-      return <h4 className={headerClasses}>{text}</h4>;
+      return <h4 className={headerClasses}>{children}</h4>;
     case 3:
-      return <h3 className={headerClasses}>{text}</h3>;
+      return <h3 className={headerClasses}>{children}</h3>;
     case 2:
-      return <h2 className={headerClasses}>{text}</h2>;
+      return <h2 className={headerClasses}>{children}</h2>;
     case 1:
     default:
-      return <h1 className={headerClasses}>{text}</h1>;
+      return <h1 className={headerClasses}>{children}</h1>;
   }
 };
 

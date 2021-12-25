b6ab512335fa6483e56a73ca92d1cc4ab56994d5
Dipesh Patil <heydipesh28@gmail.com>
2021-12-25 15:18:08 +0530

    removed

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -14,7 +14,7 @@ import config from "../../config/config";
 
 import "./AboutPage.scss";
 
-const { appName, isDark, isDevice } = config;
+const { appName, isDark } = config;
 
 const aboutPageOptions = {
   fluidContainer: false,

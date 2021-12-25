55a420e9528c4be66926e2feb67a941c1f0a8674
Dipesh Patil <heydipesh28@gmail.com>
2021-12-25 13:29:05 +0530

    Prettier Fixes

---
+++ src/components/ModalContents/ProductModalContent/ProductModalContent.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+import config from "../../../config/config";
+
+import "./ProductModalContent.scss";
+
+const { isDevice } = config;
+
+const ProductModalContent = ({ product = {} }) => {
+  const { description, image, tags, featured } = product;
+
+  return (
+    <Container className={clsx(["p-2"])} fluid>
+      <Row>
+        <Col sm={4} md={4} lg={4}>
+          <center>
+            <Image className="featured-product-image" src={image} />
+          </center>
+        </Col>
+        <Col
+          className={clsx([
+            isDevice && "mt-3",
+            "text-justify",
+            "readex-pro",
+            "readex-pro__light",
+          ])}
+          sm={8}
+          md={8}
+          lg={8}
+        >
+          {(tags ?? []).length > 0 && (
+            <div className={clsx(["product-tags", "mb-3"])}>
+              {tags.map((tag, idx) => (
+                <span
+                  className={clsx([
+                    "product-tag",
+                    "readex-pro",
+                    "readex-pro__light",
+                  ])}
+                  key={idx}
+                >
+                  {tag.toUpperCase()}
+                </span>
+              ))}
+            </div>
+          )}
+          {description}
+          {featured && (
+            <div
+              className={clsx(["featured", "readex-pro", "readex-pro__light"])}
+            >
+              <span className="featured-tag">
+                Featured Product <i className="bi bi-check-circle-fill" />
+              </span>
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+ProductModalContent.propTypes = {
+  product: PropTypes.object.isRequired,
+};
+
+export default ProductModalContent;
